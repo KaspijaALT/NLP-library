23ac7766e1bc657e2b94fad39aba328b435fefb7
kaspijaalt <kaspars.fn@gmail.com>
2024-11-13 19:03:23 +0200

    changes

--- Task1.py
+++ Task1.py
@@ -1,7 +1,11 @@
+# Izveido programmu, kas nosaka, cik bieži katrs vārds atkārtojas tekstā.
+# Ignorē lielos burtus, lai "kaķis" un "Kaķis" tiktu uzskatīti par vienādiem.
+
+
 from collections import Counter
 
-text = """Mākoņainā dienā kaķis sēdēja uz palodzes. Kaķis domāja, kāpēc debesis ir pelēkas. 
-Kaķis gribēja redzēt sauli, bet saule slēpās aiz mākoņiem."""
+text = "Mākoņainā dienā kaķis sēdēja uz palodzes. Kaķis domāja, kāpēc debesis ir pelēkas. 
+Kaķis gribēja redzēt sauli, bet saule slēpās aiz mākoņiem."
 
 words = text.lower().split()
 word_count = Counter(words)
