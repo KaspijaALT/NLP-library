1eae1e785d99c5bc2a084ca6d1f327978b728682
kaspijaalt <kaspars.fn@gmail.com>
2024-11-13 19:00:33 +0200

    Task1

---
+++ Task1.py
@@ -0,0 +1,9 @@
+from collections import Counter
+
+text = """Mākoņainā dienā kaķis sēdēja uz palodzes. Kaķis domāja, kāpēc debesis ir pelēkas. 
+Kaķis gribēja redzēt sauli, bet saule slēpās aiz mākoņiem."""
+
+words = text.lower().split()
+word_count = Counter(words)
+
+print(word_count)
