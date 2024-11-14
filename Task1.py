38a575c406b222ef7d3bdb36310636ddf545c116
kaspijaalt <kaspars.fn@gmail.com>
2024-11-14 21:10:39 +0200

    task 3

--- Task1.py
+++ Task1.py
@@ -4,8 +4,8 @@
 
 from collections import Counter
 
-text = "Mākoņainā dienā kaķis sēdēja uz palodzes. Kaķis domāja, kāpēc debesis ir pelēkas. 
-Kaķis gribēja redzēt sauli, bet saule slēpās aiz mākoņiem."
+text = """Mākoņainā dienā kaķis sēdēja uz palodzes. Kaķis domāja, kāpēc debesis ir pelēkas. 
+Kaķis gribēja redzēt sauli, bet saule slēpās aiz mākoņiem."""
 
 words = text.lower().split()
 word_count = Counter(words)
